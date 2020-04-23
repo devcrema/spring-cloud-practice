b9e27819b00b8c0ed369baa1be3f1965cde4d522
devcrema <devcrema@gmail.com>
2020-04-23 20:34:01 +0900

    add client configuration, add server config.

---
+++ article_service/src/main/java/devcrema/article_service/RefreshProfileTestController.java
@@ -0,0 +1,22 @@
+package devcrema.article_service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/refresh/test")
+@RefreshScope
+public class RefreshProfileTestController {
+    @Value("${server.port}")
+    private int port;
+    @Value("${spring.message}")
+    private String message;
+
+    @GetMapping("")
+    public String refreshTest() {
+        return "refresh - Port " + port + " - " + message;
+    }
+}
